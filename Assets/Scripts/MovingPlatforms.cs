0613ea7151a21ad7268cbbc3b93a07e3b07b5aba
Etheratic <136394417+Etheratic@users.noreply.github.com>
2023-11-14 18:56:28 +1100

    14/11

--- Assets/Scripts/MovingPlatforms.cs
+++ Assets/Scripts/MovingPlatforms.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MovingPlatforms : MonoBehaviour
 {
+    public Transform[] MoveToPos;
+   
+
     // Start is called before the first frame update
     void Start()
     {
