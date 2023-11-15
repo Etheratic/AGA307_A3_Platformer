61609b83a4c13e19a5bdc4cb384fbaffd17b5588
Etheratic <136394417+Etheratic@users.noreply.github.com>
2023-11-15 19:33:52 +1100

    moving platforms start

--- Assets/Scripts/MovingPlatforms.cs
+++ Assets/Scripts/MovingPlatforms.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatforms : MonoBehaviour
+
+public class MovingPlatforms : GameBehaviour
 {
+    public Transform[] moveToPos;
+    public GameObject[] movingPlatforms;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    IEnumerator Move()
     {
         
     }
